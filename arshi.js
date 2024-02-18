981c84c652bd585224615d74c6d599fc7934e03d
Arshiyan Ali <155827717+Arshiyan-ali@users.noreply.github.com>
2024-02-18 17:15:24 +0500

    Add files via upload

---
+++ arshi.js
@@ -0,0 +1,26 @@
+const display = document.querySelector('.counter-preview');
+const allBtns = document.querySelector('#allBtns');
+console.log(allBtns)
+
+
+
+
+allBtns.addEventListener('click', counter);
+
+
+let value = 0;
+function counter(e){
+    const btn = e.target.id;
+    if(btn === 'increment'){
+        value += 1;
+    }
+    else if(
+        btn === 'decrement'
+    ){
+        value -= 1;
+    }
+    else{
+        value=0;
+    }
+    display.textContent = value;
+}
